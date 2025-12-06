19f4394efd8d87898f0b4adb676a98db246b0d82
Danielle Cushing <dcushing09@gmail.com>
2025-12-06 11:24:12 -0600

    Initial Commit

---
+++ CrossStitchStashTests/CrossStitchStashTests.swift
@@ -0,0 +1,16 @@
+//
+//  CrossStitchStashTests.swift
+//  CrossStitchStashTests
+//
+//  Created by Danielle Cushing1 on 12/6/25.
+//
+
+import Testing
+
+struct CrossStitchStashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
